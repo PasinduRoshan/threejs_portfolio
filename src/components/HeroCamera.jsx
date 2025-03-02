292b89b4f535b879dfb3e4b8003d99a887febe99
Pasinduroshan <roshanpasindu@gmail.com>
2025-03-02 07:27:38 +0530

    first commit

---
+++ src/components/HeroCamera.jsx
@@ -0,0 +1,19 @@
+import { useFrame } from '@react-three/fiber';
+import React from 'react'
+import { useRef } from 'react';
+import { easing } from 'maath';
+
+const HeroCamera = ({children, isMobile}) => {
+    const groupRef = useRef();
+
+    useFrame((state, delta) =>{
+        easing.damp3(state.camera.position,[0, 0, 20],0.25, delta);
+    })
+
+
+  return (
+    <group ref={groupRef}>{children}</group>
+  )
+}
+
+export default HeroCamera
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,59 @@
+import { PerspectiveCamera } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
+import HackerRoom from "../components/HackerRoom"
+import { Suspense } from "react"
+import CanvasLoader from "../components/CanvasLoader"
+import { DirectionalLight } from "three"
+import { Leva, useControls } from "leva"
+import { CSS3DObject } from "three/examples/jsm/Addons.js"
+import { useMediaQuery } from "react-responsive"
+import { calculateSizes } from "../constants"
+import Target from "../components/Target.jsx"
+import ReactLogo from "../components/ReactLogo.jsx"
+import Cube from "../components/Cube.jsx"
+import Rings from "../components/Rings.jsx"
+import HeroCamera from "../components/HeroCamera.jsx"
+
+
+const Hero = () => {
+    const isSmall = useMediaQuery({maxWidth: 480});
+    const isMobile = useMediaQuery({maxWidth:768});
+    const isTablet = useMediaQuery({minWidth: 768, maxWidth: 1024});
+
+    const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">hero
+    <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">Hi, I am Roshan <span className="waving-hand">👋</span></p>
+        <p className="hero_tag text-gray_gradient">Building Websites & Mobile Apps</p>
+    </div>
+    <div className="w-full h-full absolute inset-0">
+    
+        <Canvas className="w-full h-full">
+            <Suspense fallback = {<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0,0,20]}/>
+            <HeroCamera>
+            <HackerRoom 
+            position={sizes.deskPosition}
+            rotation={[0, -Math.PI, 0]}
+            scale={sizes.deskScale} />
+            </HeroCamera>
+
+            <group>
+                <Target position = {sizes.targetPosition}></Target>
+                <ReactLogo position ={sizes.reactLogoPosition} />
+                <Cube position = {sizes.cubePosition}/>
+                <Rings position = {sizes.ringPosition}/>
+            </group>
+
+            <ambientLight intensity={1}/>
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+            </Suspense>
+        </Canvas>
+    </div>
+    </section>
+  )
+}
+
+export default Hero
